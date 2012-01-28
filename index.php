daff933620495ffeae913196c5c750f2cedea314
David Stump <david@davidstump.net>
2012-01-28 13:28:44 -0500

    added in popup function for posting to wall

--- index.php
+++ index.php
@@ -132,6 +132,18 @@ $birthyear = 2011;
 	  /* change it to what suits you! */
 	</script>
 	<![endif]-->
+	
+	<script>
+      function popup(pageURL, title,w,h) {
+        var left = (screen.width/2)-(w/2);
+        var top = (screen.height/2)-(h/2);
+        var targetWin = window.open(
+          pageURL,
+          title,
+          'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left
+          );
+      }
+    </script>
 </head>
 <body>
 <body>
